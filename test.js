2bcaa586591c8c30e451e9fdcb66a35861159403
Ricardo-Silva91 <ricardos897@hotmail.com>
2017-05-06 17:17:10 +0000

    added test for issue #43 case

--- test.js
+++ test.js
@@ -24,6 +24,8 @@ test('main', t => {
 	t.is(m('http://sindresorhus.com////foo////bar'), 'http://sindresorhus.com/foo/bar');
 	t.is(m('//sindresorhus.com/', {normalizeProtocol: false}), '//sindresorhus.com');
 	t.is(m('//sindresorhus.com:80/', {normalizeProtocol: false}), '//sindresorhus.com');
+	t.is(m('file:///foo/node_modules/%40angular/http'), 'file:///foo/node_modules/@angular/http');
+	t.is(m('http://example.com/%40dave'), 'http://example.com/@dave');
 	t.is(m('http://sindresorhus.com/foo#bar'), 'http://sindresorhus.com/foo');
 	t.is(m('http://sindresorhus.com/foo#bar', {stripFragment: false}), 'http://sindresorhus.com/foo#bar');
 	t.is(m('http://sindresorhus.com/foo/bar/../baz'), 'http://sindresorhus.com/foo/baz');
