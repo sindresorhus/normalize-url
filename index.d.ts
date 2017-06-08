b87f53c069cec2ccdc0c779701407034e39b1de8
Denis Sedchenko <ascii@live.ru>
2017-06-08 12:12:26 +0300

    Add TypeScript types

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,17 @@
-test
+/// Type definitions for normalize-url
+
+declare module NormalizeUrl {
+  interface INormalizeUrlArgs {
+		normalizeProtocol: boolean;
+		normalizeHttps: boolean;
+		stripFragment: boolean;
+		stripWWW: boolean;
+		removeQueryParameters: string[];
+		removeTrailingSlash: boolean;
+		removeDirectoryIndex: RegExp[];
+	}
+								
+	function normalizeUrl(url: string, args?: INormalizeUrlArgs): string;
+}
+
+export = NormalizeUrl.normalizeUrl;
