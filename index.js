a514ad032096404fe0fce995777b176190d73a5f
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-05-09 12:32:50 +0200

    Use WHATWG URL compliance

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
 'use strict';
-const url = require('url');
-const punycode = require('punycode');
+const {URL} = require('url');
 const queryString = require('query-string');
 const prependHttp = require('prepend-http');
 const sortKeys = require('sort-keys');
-
-const DEFAULT_PORTS = {
-	'http:': 80,
-	'https:': 443,
-	'ftp:': 21
-};
-
-// Protocols that always contain a `//`` bit
-const slashedProtocol = {
-	http: true,
-	https: true,
-	ftp: true,
-	gopher: true,
-	file: true,
-	'http:': true,
-	'https:': true,
-	'ftp:': true,
-	'gopher:': true,
-	'file:': true
-};
 
 function testParameter(name, filters) {
 	return filters.some(filter => filter instanceof RegExp ? filter.test(name) : filter === name);
@@ -41,38 +20,20 @@ module.exports = (str, opts) => {
 		sortQueryParameters: true
 	}, opts);
 
-	if (typeof str !== 'string') {
-		throw new TypeError('Expected a string');
-	}
-
 	const hasRelativeProtocol = str.startsWith('//');
 
 	// Prepend protocol
 	str = prependHttp(str.trim()).replace(/^\/\//, 'http://');
 
-	const urlObj = url.parse(str);
+	const urlObj = new URL(str);
 
 	if (opts.normalizeHttps && urlObj.protocol === 'https:') {
 		urlObj.protocol = 'http:';
 	}
 
-	if (!urlObj.hostname && !urlObj.pathname) {
-		throw new Error('Invalid URL');
-	}
-
-	// Prevent these from being used by `url.format`
-	delete urlObj.host;
-	delete urlObj.query;
-
 	// Remove fragment
 	if (opts.stripFragment) {
-		delete urlObj.hash;
-	}
-
-	// Remove default port
-	const port = DEFAULT_PORTS[urlObj.protocol];
-	if (Number(urlObj.port) === port) {
-		delete urlObj.port;
+		urlObj.hash = '';
 	}
 
 	// Remove duplicate slashes
@@ -100,17 +61,7 @@ module.exports = (str, opts) => {
 		}
 	}
 
-	// Resolve relative paths, but only for slashed protocols
-	if (slashedProtocol[urlObj.protocol]) {
-		const domain = urlObj.protocol + '//' + urlObj.hostname;
-		const relative = url.resolve(domain, urlObj.pathname);
-		urlObj.pathname = relative.replace(domain, '');
-	}
-
 	if (urlObj.hostname) {
-		// IDN to Unicode
-		urlObj.hostname = punycode.toUnicode(urlObj.hostname).toLowerCase();
-
 		// Remove trailing dot
 		urlObj.hostname = urlObj.hostname.replace(/\.$/, '');
 
@@ -118,11 +69,6 @@ module.exports = (str, opts) => {
 		if (opts.stripWWW) {
 			urlObj.hostname = urlObj.hostname.replace(/^www\./, '');
 		}
-	}
-
-	// Remove URL with empty query string
-	if (urlObj.search === '?') {
-		delete urlObj.search;
 	}
 
 	const queryParameters = queryString.parse(urlObj.search);
@@ -141,13 +87,8 @@ module.exports = (str, opts) => {
 		urlObj.search = queryString.stringify(sortKeys(queryParameters));
 	}
 
-	// Decode query parameters
-	if (urlObj.search !== null) {
-		urlObj.search = decodeURIComponent(urlObj.search);
-	}
-
 	// Take advantage of many of the Node `url` normalizations
-	str = url.format(urlObj);
+	str = urlObj.toString();
 
 	// Remove ending `/`
 	if (opts.removeTrailingSlash || urlObj.pathname === '/') {
