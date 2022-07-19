141388e267d02563ea1472ffdf0ac006c5bd7def
Sindre Sorhus <sindresorhus@gmail.com>
2022-07-19 15:30:47 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -201,7 +201,7 @@ export interface Options {
 	readonly removeTrailingSlash?: boolean;
 
 	/**
-	Remove a sole `/` pathname in the output. This option is independant of `removeTrailingSlash`.
+	Remove a sole `/` pathname in the output. This option is independent of `removeTrailingSlash`.
 
 	@default true
 
